4bc552d9a61f26518f64525e4fb33dbadb1c1a65
Kevin Ho <goldapple820@gmail.com>
2023-10-05 17:52:50 +0900

    earn modify..

--- offerwall/resources/views/earn.blade.php
+++ offerwall/resources/views/earn.blade.php
@@ -2,203 +2,154 @@
 
 @section('content')
 <link href="{{ asset('assets/css/earn.css') }}" rel="stylesheet"  type="text/css" />
-<div class="container">
-    <div id="slide-left-container">
-      <div class="slide-left">
-      </div>
-    </div>
-    <div id="cards-container">
-      <div class="cards">
-        <div class="card">
-          <img src="http://via.placeholder.com/220x220" alt="Animals" style="width:100%">
-          <div class="container">
-            <h4>
-              <b>Animals</b>
-            </h4>
-          </div>
-        </div>
-        <div class="card">
-          <img src="http://via.placeholder.com/220x220" alt="Nature" style="width:100%">
-          <div class="container">
-            <h4>
-              <b>Nature</b>
-            </h4>
-          </div>
-        </div>
-        <div class="card">
-          <img src="http://via.placeholder.com/220x220" alt="Architecture" style="width:100%">
-          <div class="container">
-            <h4>
-              <b>Architecture</b>
-            </h4>
-          </div>
-        </div>
-        <div class="card">
-          <img src="http://via.placeholder.com/220x220" alt="Technology" style="width:100%">
-          <div class="container">
-            <h4>
-              <b>Technology</b>
-            </h4>
-          </div>
-        </div>
-        <div class="card">
-          <img src="http://via.placeholder.com/220x220" alt="People" style="width:100%">
-          <div class="container">
-            <h4>
-              <b>People</b>
-            </h4>
-          </div>
-        </div>
-        <div class="card">
-          <img src="http://via.placeholder.com/220x220" alt="Animals" style="width:100%">
-          <div class="container">
-            <h4>
-              <b>Animals</b>
-            </h4>
-          </div>
-        </div>
-        <div class="card">
-          <img src="http://via.placeholder.com/220x220" alt="Nature" style="width:100%">
-          <div class="container">
-            <h4>
-              <b>Nature</b>
-            </h4>
-          </div>
-        </div>
-        <div class="card">
-          <img src="http://via.placeholder.com/220x220" alt="Architecture" style="width:100%">
-          <div class="container">
-            <h4>
-              <b>Architecture</b>
-            </h4>
-          </div>
-        </div>
-        <div class="card">
-          <img src="http://via.placeholder.com/220x220" alt="Technology" style="width:100%">
-          <div class="container">
-            <h4>
-              <b>Technology</b>
-            </h4>
-          </div>
-        </div>
-        <div class="card">
-          <img src="http://via.placeholder.com/220x220" alt="People" style="width:100%">
-          <div class="container">
-            <h4>
-              <b>People</b>
-            </h4>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <div id="slide-right-container">
-      <div class="slide-right">
+<div class="contaner" style="margin-top: 200px;">
+  <div class="row" style="display: flex;justify-content:center;">
+    <div class="col-sm-8">
+      <p style="font-size: 30px">Featured Offers</p>
+    </div>
+  </div>
+  <div class="row" style="display: flex;justify-content:center;">
+    <div class="col-sm-10">
+      <div class="econtainer">
+        <div id="slide-left-container">
+          <div class="slide-left">
+          </div>
+        </div>
+        <div id="ecards-container">
+          <div class="ecards">
+            <div class="ecard">
+              <div class="center">
+                <img  class="ecard-img " src="http://via.placeholder.com/220x220" alt="Animals" >
+              </div>
+              <div class="center">
+                <p>Animals</p>
+              </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <p style="text-align: left;">0.03$</p>
+                </div>
+                <div class="col-sm-6">
+                  <p style="background-color: rgb(194, 219, 255);text-align:right">
+                    Game
+                  </p>
+                </div>
+              </div>             
+            </div>
+          </div>
+        </div>
+    
+        <div id="slide-right-container">
+          <div class="slide-right">
+          </div>
+        </div>
       </div>
     </div>
+  </div>
+  <div class="row">
 
   </div>
+  
+
+</div>
+
   <!-- Scripts -->
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
-  <script>
-    /**
- * @description Change Home page slider's arrows active status
- */
-function updateSliderArrowsStatus(
-  cardsContainer,
-  containerWidth,
-  cardCount,
-  cardWidth
-) {
-  if (
-    $(cardsContainer).scrollLeft() + containerWidth <
-    cardCount * cardWidth + 15
-  ) {
-    $("#slide-right-container").addClass("active");
-  } else {
-    $("#slide-right-container").removeClass("active");
-  }
-  if ($(cardsContainer).scrollLeft() > 0) {
-    $("#slide-left-container").addClass("active");
-  } else {
-    $("#slide-left-container").removeClass("active");
-  }
-}
-$(function() {
-  // Scroll products' slider left/right
-  let div = $("#cards-container");
-  let cardCount = $(div)
-    .find(".cards")
-    .children(".card").length;
-  let speed = 1000;
-  let containerWidth = $(".container").width();
-  let cardWidth = 250;
+  <script>      
+    function updateSliderArrowsStatus(
+    ecardsContainer,
+    containerWidth,
+    ecardCount,
+    ecardWidth
+    ) {
+    if (
+        $(ecardsContainer).scrollLeft() + containerWidth <
+        ecardCount * ecardWidth + 15
+    ) {
+        $("#slide-right-container").addClass("active");
+    } else {
+        $("#slide-right-container").removeClass("active");
+    }
+    if ($(ecardsContainer).scrollLeft() > 0) {
+        $("#slide-left-container").addClass("active");
+    } else {
+        $("#slide-left-container").removeClass("active");
+    }
+    }
+    $(function() {
+    // Scroll products' slider left/right
+    let div = $("#ecards-container");
+    let ecardCount = $(div)
+        .find(".ecards")
+        .children(".ecard").length;
+    let speed = 500;
+    let containerWidth = $(".container").width();
+    let ecardWidth = 250;
 
-  updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
+    updateSliderArrowsStatus(div, containerWidth, ecardCount, ecardWidth);
 
-  //Remove scrollbars
-  $("#slide-right-container").click(function(e) {
-    if ($(div).scrollLeft() + containerWidth < cardCount * cardWidth) {
-      $(div).animate(
-        {
-          scrollLeft: $(div).scrollLeft() + cardWidth
-        },
-        {
-          duration: speed,
-          complete: function() {
-            setTimeout(
-              updateSliderArrowsStatus(
-                div,
-                containerWidth,
-                cardCount,
-                cardWidth
-              ),
-              1005
-            );
-          }
+    //Remove scrollbars
+    $("#slide-right-container").click(function(e) {
+        if ($(div).scrollLeft() + containerWidth < ecardCount * ecardWidth) {
+        $(div).animate(
+            {
+            scrollLeft: $(div).scrollLeft() + ecardWidth
+            },
+            {
+            duration: speed,
+            complete: function() {
+                setTimeout(
+                updateSliderArrowsStatus(
+                    div,
+                    containerWidth,
+                    ecardCount,
+                    ecardWidth
+                ),
+                1005
+                );
+            }
+            }
+        );
         }
-      );
-    }
-    updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-  });
-  $("#slide-left-container").click(function(e) {
-    if ($(div).scrollLeft() + containerWidth > containerWidth) {
-      $(div).animate(
-        {
-          scrollLeft: "-=" + cardWidth
-        },
-        {
-          duration: speed,
-          complete: function() {
-            setTimeout(
-              updateSliderArrowsStatus(
-                div,
-                containerWidth,
-                cardCount,
-                cardWidth
-              ),
-              1005
-            );
-          }
+        updateSliderArrowsStatus(div, containerWidth, ecardCount, ecardWidth);
+    });
+    $("#slide-left-container").click(function(e) {
+        if ($(div).scrollLeft() + containerWidth > containerWidth) {
+        $(div).animate(
+            {
+            scrollLeft: "-=" + ecardWidth
+            },
+            {
+            duration: speed,
+            complete: function() {
+                setTimeout(
+                updateSliderArrowsStatus(
+                    div,
+                    containerWidth,
+                    ecardCount,
+                    ecardWidth
+                ),
+                1005
+                );
+            }
+            }
+        );
         }
-      );
-    }
-    updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-  });
+        updateSliderArrowsStatus(div, containerWidth, ecardCount, ecardWidth);
+    });
 
-  // If resize action ocurred then update the container width value
-  $(window).resize(function() {
-    try {
-      containerWidth = $("#cards-container").width();
-      updateSliderArrowsStatus(div, containerWidth, cardCount, cardWidth);
-    } catch (error) {
-      console.log(
-        `Error occured while trying to get updated slider container width: 
-            ${error}`
-      );
-    }
-  });
-});
-
+    // If resize action ocurred then update the container width value
+    $(window).resize(function() {
+        try {
+        containerWidth = $("#ecards-container").width();
+        updateSliderArrowsStatus(div, containerWidth, ecardCount, ecardWidth);
+        } catch (error) {
+        console.log(
+            `Error occured while trying to get updated slider container width: 
+                ${error}`
+        );
+        }
+    });
+    });
   </script>
 @endsection
