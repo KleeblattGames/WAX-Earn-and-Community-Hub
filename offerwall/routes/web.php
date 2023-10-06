5885f26b63ba4888c605cb7abb91f4c05d25363b
Jonas <projonurs1216@gmil.com>
2023-10-06 10:44:27 +0900

    change the header access permission

--- offerwall/routes/web.php
+++ offerwall/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/home');
+    return redirect('/starter_zone');
 })->middleware("auth");
 Route::get('/earn', function () {
     return view('earn');
