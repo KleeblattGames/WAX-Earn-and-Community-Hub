c3771b1ac4ab299dd38282185d358feef306ae2b
Kevin Ho <goldapple820@gmail.com>
2023-10-07 12:17:36 +0900

    feature-two  postback function created

--- offerwall/app/Http/Controllers/EarnController.php
+++ offerwall/app/Http/Controllers/EarnController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use GuzzleHttp\Client;
 use Auth;
+use App\Models\Offer;
+use Carbon\Carbon;
 
 
 class EarnController extends Controller
@@ -73,6 +74,33 @@ class EarnController extends Controller
     }
     public function set_lootably_offers(Request $request)
     {
+        $user_id = $request->user_id;
+        $amount = $request->amount;
+        $payout = $request->payout;
+        $offer_id = $request->offer_id;
+        $offer_name = $request->offer_name;
+        $txn_id = $request->txn_id;
+        $status = $request->status;
+        $multi_status = $request->multi_status;
+
+        $hash = $request->hash;
+        $conversion_ip = $request->ip;
+
+
+        $offer = new Offer;
+        $offer->user_id = $user_id;
+        $offer->partner = "lootably";
+        $offer->offer_name = $offer_name;
+        $offer->offer_id = $offer_id;
+        $offer->tx_id = $txn_id;
+        $offer->ip = $conversion_ip;
+        $offer->status = $status;
+        $offer->multi_status = $multi_status;
+        $offer->payout = $payout;
+        $offer->currency_reward = $amount;
+        $currentDate = Carbon::now();
+        $offer->created_at = $currentDate;
+        $offer->save();
         return 1;
     }
     public function set_notik_offers(Request $request)
@@ -90,7 +118,25 @@ class EarnController extends Controller
         $currency_name = $request->currency_name;
         $timestamp = $request->timestamp;
         $hash = $request->hash;
+        $conversion_ip = $request->conversion_ip;
 
+
+        $offer = new Offer;
+        $offer->user_id = $user_id;
+        $offer->partner = "notik";
+        $offer->offer_name = $offer_name;
+        $offer->offer_id = $offer_id;
+        $offer->tx_id = $txn_id;
+        $offer->ip = $conversion_ip;
+        $offer->status = 0;
+        $offer->multi_status = 0;
+        $offer->payout = $payout;
+        $offer->currency_reward = $amount;
+        $currentDate = Carbon::now();
+        $formattedDate = $currentDate->format('Y-m-d');
+        $offer->created_at = $currentDate;
+        $offer->save();
+        return 1;
         /*Check if duplicate transaction*/
         $transactionExist = false; // Search in database if current txn_id exist. True if exist
         if ($transactionExist) {
@@ -114,6 +160,7 @@ class EarnController extends Controller
         /*Check if the generated hash is the same as the "hash" query parameter*/
         if ($generatedHash == $hash) {
             /*Validation successful. Queue your user credit functions and send us postback received positive response*/
+
             return 1;
         } else {
             /*Hash not equal. Send error response.
