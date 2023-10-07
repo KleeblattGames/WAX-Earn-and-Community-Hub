bb006d9e775ef86efe753cb57031679514ab33bf
Kevin Ho <goldapple820@gmail.com>
2023-10-07 11:28:01 +0900

    postback url test

--- offerwall/app/Http/Controllers/EarnController.php
+++ offerwall/app/Http/Controllers/EarnController.php
@@ -25,13 +25,13 @@ class EarnController extends Controller
         $response = Http::get($url);
         if ($response->successful()) {
             return $response->json(); // Convert the response to JSON
-            
+
             // Process the data as needed
         } else {
             // Handle the unsuccessful response
             return $response->status();
             // Handle the error accordingly
-        }        
+        }
 
     }
 
@@ -41,33 +41,86 @@ class EarnController extends Controller
         $response = Http::get($url);
         if ($response->successful()) {
             return $response->json(); // Convert the response to JSON
-            
+
             // Process the data as needed
         } else {
             // Handle the unsuccessful response
             return $response->status();
             // Handle the error accordingly
-        }         
+        }
 
     }
     public function get_iframe(Request $request)
     {
         $user_id = Auth::user()->id;
-        $lootably_url = "https://wall.lootably.com/?placementID=clhedla0e0oh101uy7xwj55vz&sid=".$user_id;
-        $notik_url = "https://notik.me/coins?api_key=tM9z2VjFRUGQ2iBdVZy43suBlzVl4a3t&pub_id=pO8AAu&app_id=ENRGtwfmUd&user_id=".$user_id;   
+        $lootably_url = "https://wall.lootably.com/?placementID=clhedla0e0oh101uy7xwj55vz&sid=" . $user_id;
+        $notik_url = "https://notik.me/coins?api_key=tM9z2VjFRUGQ2iBdVZy43suBlzVl4a3t&pub_id=pO8AAu&app_id=ENRGtwfmUd&user_id=" . $user_id;
         $url = "";
-        if($request->partner == "lootably") $url = $lootably_url;
-        else $url = $notik_url;
+        if ($request->partner == "lootably")
+            $url = $lootably_url;
+        else
+            $url = $notik_url;
         $response = Http::get($url);
         if ($response->successful()) {
             return $response->body(); // Convert the response to JSON
-            
+
             // Process the data as needed
         } else {
             // Handle the unsuccessful response
             return $response->status();
             // Handle the error accordingly
-        }      
+        }
+    }
+    public function set_lootably_offers(Request $request)
+    {
+        return 1;
+    }
+    public function set_notik_offers(Request $request)
+    {
+        $pub_id = $request->pub_id;
+        $app_id = $request->app_id;
+        $user_id = $request->user_id;
+        $amount = $request->amount;
+        $payout = $request->payout;
+        $offer_id = $request->offer_id;
+        $offer_name = $request->offer_name;
+        $event_id = $request->event_id;
+        $event_name = $request->event_name;
+        $txn_id = $request->txn_id;
+        $currency_name = $request->currency_name;
+        $timestamp = $request->timestamp;
+        $hash = $request->hash;
+
+        /*Check if duplicate transaction*/
+        $transactionExist = false; // Search in database if current txn_id exist. True if exist
+        if ($transactionExist) {
+            /*Duplicate transaction detected. Do not reward user but send us postback received positive response*/
+            return 1;
+        }
+
+        /*Create validation hash and validate hashes*/
+        $secretKey = "ENRGtwfmUd"; // This has to be your App's secret key that you can find in you App detail page
+        /*Get the currently active http protocol*/
+        $protocol = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] === "on") ? "https" : "http";
+        /*Build the full callback URL*/
+        /*Example: https://url.com?param1=foo&param2=bar&hash=3171f6b78e06cadcec4c9c3b15f8588400e8738*/
+        $url = "$protocol://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        /*Get the callback URL without the "hash" query parameter*/
+        /*Example: https://url.com?param1=foo&param2=bar*/
+        $urlWithoutHash = substr($url, 0, -strlen("&hash=$hash"));
+        /*Generate a hash from the complete callback URL without the "hash" query parameter*/
+        $generatedHash = hash_hmac("sha1", $urlWithoutHash, $secretKey);
+
+        /*Check if the generated hash is the same as the "hash" query parameter*/
+        if ($generatedHash == $hash) {
+            /*Validation successful. Queue your user credit functions and send us postback received positive response*/
+            return 1;
+        } else {
+            /*Hash not equal. Send error response.
+            Try to fix any errors found for hash validation.
+            Contact us in case the postback is from our ip and need some assistance.*/
+            return 0;
+        }
     }
     /**
      * Show the form for creating a new resource.
