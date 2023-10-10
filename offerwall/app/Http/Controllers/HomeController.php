fabf304ca469192d00243bf4d2072acf2d1a0ca1
Jonas <projonurs1216@gmil.com>
2023-10-10 16:28:37 +0900

    feature - session manage

--- offerwall/app/Http/Controllers/HomeController.php
+++ offerwall/app/Http/Controllers/HomeController.php
@@ -18,6 +18,9 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $session = $request->session();
+        // ($session->has('first_visit')) ? $session->put('first_visit', false) : $session->put('first_visit', true);
+        // return view('first_time', ['first_visit' => $session->get('first_visit')]);
         return view('home');
     }
 }
